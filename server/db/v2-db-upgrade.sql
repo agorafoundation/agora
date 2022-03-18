1ed22f29f4b2d2d8ed830acd8b0a8d5b5505526a
Brian Gormanly <bgormanly@gmail.com>
2022-03-18 10:44:04 -0400

    a change

--- server/db/v2-db-upgrade.sql
+++ server/db/v2-db-upgrade.sql
@@ -7,5 +7,5 @@ ALTER TABLE assessments ADD COLUMN post_threshold INTEGER DEFAULT 70;
 
 -- #41 https://github.com/briangormanly/agora/issues/41
 ALTER TABLE completed_assessment RENAME COLUMN pre_post TO topic_assessment_number;
-ALTER TABLE completed_assessment ADD COLUMN percentage_correct DECIMAL(4,3);            -- existing rows will need to be computed as they are already saved
-ALTER TABLE completed_assessment ADD COLUMN completion_time TIMESTAMP;                  -- existing rows should be set to match create_time
+ALTER TABLE completed_assessment ADD COLUMN percentage_correct DECIMAL(4,3);            -- existing rows will need to be computed as they are already saved (maybe leave null then have code respond to null and show message?)
+ALTER TABLE completed_assessment ADD COLUMN completion_time TIMESTAMP;                  -- existing rows should be set to match create_time (or leave null, see above)
