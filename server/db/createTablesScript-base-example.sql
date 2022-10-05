fa0ed5e88ead9d75573f0aea5017d3b46e2dd3d2
chfioti2201 <christopher.fioti@Nielsen.com>
2022-10-05 13:19:18 -0400

    Wrote a couple lines to generate dummy data into the discussion tables

--- server/db/createTablesScript-base-example.sql
+++ server/db/createTablesScript-base-example.sql
@@ -604,3 +604,8 @@ CREATE TABLE IF NOT EXISTS discussion_comment_ratings (
 );
 
 GRANT ALL PRIVILEGES ON TABLE discussion_comment_ratings TO agora;
+
+-- Generating some dummy data
+INSERT INTO discussions (parent_id, parent_type, discussion_text) VALUES (0, 'topic', 'Hello! This is a topic'), (1, 'goal', 'Hello! This is a goal!');
+INSERT INTO discussion_comments (comment_id, parent_id, parent_type, comment_text, user_id) VALUES (0, 0, 'topic', 'Hi! I am commenting on this topic!', 12), (1, 1, 'goal', 'Hi! I am commenting on this goal!', 13);
+INSERT INTO discussion_comment_ratings (comment_id, user_id, rating) VALUES (3, 12, TRUE), (2, 13, TRUE), (1, 24, FALSE), (0, 36, FALSE), (1, 0, TRUE), (2, 9, FALSE), (0, 13, TRUE), (1, 12, FALSE);
