e2d29e0c12a16ae208554d1f05750c6dfe8c98c0
Tones55 <54862709+Tones55@users.noreply.github.com>
2022-10-07 14:25:51 -0400

    finish deleteComment

--- server/service/discussionService.js
+++ server/service/discussionService.js
@@ -163,7 +163,6 @@ exports.editComment = async (commentId , userId , editedComment) => {
         return false;
     }
 
-    //compare to see if the user created the comment they are trying to edit
     if  (commentAuthorId == userId) {
 
         try {
@@ -171,7 +170,7 @@ exports.editComment = async (commentId , userId , editedComment) => {
             const values = [editedComment.body , commentId]
 
             let res = await db.query(text , values)
-            return true
+            return res.rowCount == 1
         }
         catch(e) {
             console.log(e.stack);
@@ -185,13 +184,29 @@ exports.editComment = async (commentId , userId , editedComment) => {
 
 exports.deleteComment = async (commentId , userId) => {
 
-    //query database for the userid associated with the comment
-    commentAuthorId = 3
+    try {
+        const text = `SELECT user_id from discussion_comments WHERE comment_id = $1`
+        const values = [commentId]
 
-    if  (commentAuthorId == commentId) {
+        let res = await db.query(text , values)
 
-        //query database to delete comment
-        return {commentId , userId}
+        if(res.rows.length === 0) {
+            return {error: "No such comment with id: " + commentId}
+        }
+
+        commentAuthorId = res.rows[0].user_id
+    }
+    catch(e) {
+        console.log(e.stack);
+        return false;
+    }
+
+    if  (commentAuthorId == userId) {
+        const text = `DELETE FROM discussion_comments WHERE comment_id = $1`
+        const values = [commentId]
+
+        let res = await db.query(text , values)
+        return res.rowCount == 1
     }
     else {
         return {error: "User not allowed to delete this comment"}
