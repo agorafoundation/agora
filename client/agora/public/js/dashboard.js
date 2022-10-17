82d7c56d5b6b36646e14b5c1f0433af18d42b6a5
RichieConsiglio <richard.consiglio1@marist.edu>
2022-10-17 12:53:52 -0400

    add right click sub menu to dashboard

--- client/agora/public/js/dashboard.js
+++ client/agora/public/js/dashboard.js
@@ -43,6 +43,22 @@ function toggleSunEditor() {
       "none";
   }
 }
+//js for right click sub menu
+var notepad = document.getElementById("notepad");
+notepad.addEventListener("contextmenu",function(event){
+    event.preventDefault();
+    var ctxMenu = document.getElementById("ctxMenu");
+    ctxMenu.style.display = "block";
+    ctxMenu.style.left = (event.pageX - 10)+"px";
+    ctxMenu.style.top = (event.pageY - 10)+"px";
+},false);
+notepad.addEventListener("click",function(event){
+    var ctxMenu = document.getElementById("ctxMenu");
+    ctxMenu.style.display = "";
+    ctxMenu.style.left = "";
+    ctxMenu.style.top = "";
+},false);
+
 
 // sun editor for resource
 let resourceEditor = null;
