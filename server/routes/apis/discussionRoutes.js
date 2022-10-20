14710e99819c8402f738e9c12877397e3965698c
Kyle DePace <ikikomonkey@gmail.com>
2022-10-20 15:04:08 -0400

    Update server/routes/apis/discussionRoutes.js
    Co-authored-by: C. Marcus DiMarco <79232853+cmarcusdimarco@users.noreply.github.com>

--- server/routes/apis/discussionRoutes.js
+++ server/routes/apis/discussionRoutes.js
@@ -21,11 +21,11 @@ const discussionController = require( '../../controller/apis/discussionControlle
  // /api/v1/auth/discussions/goal/4 req.params.type === "goal" req.params.id === 4
 // discussions /api/v1/auth/discussions/{goal | topic}/:id
 router.route( '/goal/:id' )
-    // get a discussion based off topic id
+    // get a discussion based off goal id
     .get( async ( req, res ) => {
         discussionController.getDiscussionByGoalId( req, res );
     })
-    // update a discussion based off topic id
+    // update a discussion based off goal id
     .patch( async ( req, res ) => { 
         discussionController.updateDiscussionByGoalId( req, res );
     })
@@ -34,11 +34,11 @@ router.route( '/goal/:id' )
     })
 
 router.route( '/topic/:id' )
-    // get a discussion based off goal id
+    // get a discussion based off topic id
     .get( async ( req, res ) => {
         discussionController.getDiscussionByTopicId( req, res );
     })
-    // update a discussion based off goal id
+    // update a discussion based off topic id
     .patch( async ( req, res ) => { 
         discussionController.updateDiscussionByTopicId( req, res );
     })
