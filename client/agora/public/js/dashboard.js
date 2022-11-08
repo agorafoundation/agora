6859c59e14d4125610ce3c95d68a91f7d9da4dae
MaxEnglish <maxjenglish@gmail.com>
2022-11-08 16:35:12 -0500

    changing redundant name

--- client/agora/public/js/dashboard.js
+++ client/agora/public/js/dashboard.js
@@ -853,7 +853,7 @@ const copyLink = ( e ) => {
 };
 
 
-var newTabCards = document
+var copyLinkCards = document
     .querySelectorAll( "#copy-link-card" )
     .forEach( ( copyLinkCard ) => {
         copyLinkCard.addEventListener( "click", copyLink );
