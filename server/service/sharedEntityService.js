b703971c868a2ed80472a353a660db7e72d23a5c
Tyler Hernandez <tylerh6201@gmail.com>
2022-12-01 17:59:18 -0500

    updates insertOrUpdateSharedEntity function

--- server/service/sharedEntityService.js
+++ server/service/sharedEntityService.js
@@ -44,31 +44,45 @@ exports.insertOrUpdateSharedEntity = async ( sharedEntity ) => {
 
     const s = sharedEntity;
 
-    let text = "";
-    let values = [ s.entity_id, s.entity_type, s.share_user_id, s.owner_user_id, s.permission_level, s.can_copy ];
-
     // New entry to be added to our table. Retrieve our shared_entity_id after to inform our user.
     if ( sharedEntity.shared_entity_id == -1 ) {
-        text = "INSERT INTO shared_entities (entity_id, entity_type, share_user_id, owner_user_id, permission_level, can_copy) VALUES ($1, $2, $3, $4, $5, $6) returning shared_entity_id;";
+        let text = "INSERT INTO shared_entities (entity_id, entity_type, share_user_id, owner_user_id, permission_level, can_copy) VALUES ($1, $2, $3, $4, $5, $6) returning shared_entity_id;";
+        let values = [ s.entity_id, s.entity_type, s.share_user_id, s.owner_user_id, s.permission_level, s.can_copy ];
+
+
+        try {
+            let res = await db.query( text, values );
+
+            if( res.rowCount > 0 ) {
+                const sharedEntityId = res.rows[0].shared_entity_id;
+                return sharedEntityId;
+            }
+
+        }
+        catch ( e ) {
+            console.log( "There was an error with query " + text + "\n\n" + e );
+            return;
+        }
+    
     }
     // Update our table with new values specified in sharedEntity.
     else {
-        text = "UPDATE shared_entities SET entity_id = $1, entity_type = $2, share_user_id = $3, owner_user_id = $4, permission_level = $5, can_copy = $6 WHERE shared_entity_id = $7;";
-        values.push( s.shared_entity_id );
-    }
+        let text = "UPDATE shared_entities SET entity_id = $1, entity_type = $2, share_user_id = $3, owner_user_id = $4, permission_level = $5, can_copy = $6 WHERE shared_entity_id = $7;";
+        let values = [ s.entity_id, s.entity_type, s.share_user_id, s.owner_user_id, s.permission_level, s.can_copy, s.shared_entity_id, ];
 
-    try {
-        let res = await db.query( text, values );
+        console.log( values );
+        try {
+            let res = await db.query( text, values );
 
-        if( res.rowCount > 0 ) {
-            const sharedEntityId = res.rows[0].shared_entity_id;
-            return sharedEntityId;
-        }
+            if( res.rowCount > 0 ) {
+                const sharedEntityId = res.rows[0].shared_entity_id;
+                return sharedEntityId;
+            }
         
-
-    }
-    catch ( e ) {
-        console.log( "There was an error with query " + text + "\n\n" + e );
-        return;
+        }
+        catch ( e ) {
+            console.log( "There was an error with query " + text + "\n\n" + e );
+            return;
+        }
     }
 };
