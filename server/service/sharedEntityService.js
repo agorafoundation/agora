58b789adac4a09ba258abe26b1d9721418703b37
Tyler Hernandez <tylerh6201@gmail.com>
2022-12-01 14:15:12 -0500

    removes share_type

--- server/service/sharedEntityService.js
+++ server/service/sharedEntityService.js
@@ -47,14 +47,14 @@ exports.insertOrUpdateSharedEntity = async ( sharedEntity ) => {
 
     // New entry to be added to our table. Retrieve our shared_entity_id after to inform our user.
     if ( sharedEntity.entity_id == -1 ) {
-        text = "INSERT INTO shared_entities (shared_entity_id, entity_id, entity_type, share_user_id, owner_user_id, share_type, permission_level, can_copy) VALUES ($2, $3, $4, $5, $6, $7, $8, $9) returning shared_entity_id";
+        text = "INSERT INTO shared_entities (shared_entity_id, entity_id, entity_type, share_user_id, owner_user_id, permission_level, can_copy) VALUES ($2, $3, $4, $5, $6, $7, $8) returning shared_entity_id";
     }
     // Update our table with new values specified in sharedEntity.
     else {
-        text = "UPDATE shared_entities SET shared_entity_id = $2, entity_id = $3, entity_type = $4, share_user_id = $5, owner_user_id = $6, share_type = $7, permission_level = $8, can_copy = $9 WHERE shared_entity_id = $1;";
+        text = "UPDATE shared_entities SET shared_entity_id = $2, entity_id = $3, entity_type = $4, share_user_id = $5, owner_user_id = $6, permission_level = $7, can_copy = $8 WHERE shared_entity_id = $1;";
     }
 
-    let values = [ s.id, s.shared_entity_id, s.entity_id, s.entity_type, s.share_user_id, s.owner_user_id, s.share_type, s.permission_level, s.can_copy ];
+    let values = [ s.id, s.shared_entity_id, s.entity_id, s.entity_type, s.share_user_id, s.owner_user_id, s.permission_level, s.can_copy ];
 
     try {
         let res = await db.query( text, values );
