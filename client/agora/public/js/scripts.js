bfce6f7c4673b17e945630f7414aacb5fb374162
Gjidodaj02 <jozef.gjidoda1@marist.edu>
2023-10-13 19:11:46 -0400

    Added freinds page css, start "friends-content"

--- client/agora/public/js/scripts.js
+++ client/agora/public/js/scripts.js
@@ -46,11 +46,13 @@ function toggleSidebar() {
     if (!sideBarStatus) {
         document.getElementById("agoraSideBar").style.width = "250px";
         document.querySelector(".dashboard-content").style.marginLeft = "250px";
+        document.querySelector(".friends-content").style.marginLeft = "250px";
     }
     else {
         document.getElementById("agoraSideBar").style.width = "85px";
         if (window.innerWidth > 992) {
             document.querySelector(".dashboard-content").style.marginLeft = "85px";
+            document.querySelector(".friends-content").style.marginLeft = "85px";
         }
     }
     sideBarStatus = !sideBarStatus;
@@ -62,6 +64,7 @@ function toggleSidebar() {
 function lockSidebar() {
     if (!clientSettings.sideBarLocked) {
         document.querySelector(".dashboard-content").style.marginLeft = "250px";
+        document.querySelector(".friends-content").style.marginLeft = "250px";
 
         document.getElementById("agoraSideBar").removeEventListener("mouseenter", toggleSidebar);
         document.getElementById("agoraSideBar").removeEventListener("mouseleave", toggleSidebar);
@@ -96,6 +99,7 @@ function loadClientSettings() {
 
         if (clientSettings.sideBarLocked) {
             document.querySelector(".dashboard-content").style.marginLeft = "250px";
+            document.querySelector(".friends-content").style.marginLeft = "250px";
 
             document.getElementById("agoraSideBar").removeEventListener("mouseenter", toggleSidebar);
             document.getElementById("agoraSideBar").removeEventListener("mouseleave", toggleSidebar);
