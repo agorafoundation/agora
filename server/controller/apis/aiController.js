2ebc01768ced03654ad98892d06f617d326adbf1
B-cabrera <brendenc29@gmail.com>
2023-10-16 16:33:50 -0400

    create function to parse & split resource content

--- server/controller/apis/aiController.js
+++ server/controller/apis/aiController.js
@@ -12,3 +12,20 @@
 exports.callOpenAI = async ( req, res ) => {
 
 };
+
+// helper logic
+function parseResourceContentHtml ( content ) {
+
+    // getting all paragraphs by spliting by <p> tag
+    const paragraphs = content.split( '<p>' );
+
+    // removing all html tags in individual strings
+    for ( var i = 0; i < paragraphs.length; i++ ) {
+        paragraphs[i] = paragraphs[i].replace( /<[^>]*>/g, '' ).trim();
+    }
+
+    // shifting array to ignore first empty string
+    paragraphs.shift();
+
+    // TODO: pick out first paragraph that meets length requirement
+}
