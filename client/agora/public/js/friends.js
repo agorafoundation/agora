5b364e8b63371f4195d31050b1de76f01c418168
MichaelLaffan1 <michaellaff10876@gmail.com>
2023-10-18 23:58:56 -0400

    Get username and cleanup
    Wrote a function that gets user info by username and outputs it to console. Also did some cleanup on controlbar.ejs for the addfriends page

--- client/agora/public/js/friends.js
+++ client/agora/public/js/friends.js
@@ -2,3 +2,16 @@
 const addFriendPage = () => {
     window.location.href = "/add-friends";
 };
+
+
+const userSearch = document.getElementById('user-search');
+const searchButton = document.getElementById('btn-search');
+
+searchButton.addEventListener('click', queryUser = () => {
+    const response = fetch( "api/v1/auth//user/username/" + userSearch.value, {
+        method: "GET",
+        headers: { "Content-Type": "application/json" },
+    } )
+        .then( ( response ) => response.json() )
+    console.log(response);
+})
