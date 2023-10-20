2a16e25cdbef42616e39d9465278135dc9417ad6
MichaelLaffan1 <michaellaff10876@gmail.com>
2023-10-20 19:25:26 -0400

    Add display user functionality
    Added functionality for users to be displayed on add friends page and prevent duplicate ones from appearing.

--- client/agora/public/js/friends.js
+++ client/agora/public/js/friends.js
@@ -3,15 +3,29 @@ const addFriendPage = () => {
     window.location.href = "/add-friends";
 };
 
-
+var displayedUsers = new Set();
 const userSearch = document.getElementById( 'user-search' );
 const searchButton = document.getElementById( 'btn-search' );
+const userContainer = document.getElementById('grid-container');
 
-searchButton.addEventListener( 'click', queryUser = () => {
-    const response = fetch( "api/v1/auth//user/username/" + userSearch.value, {
+searchButton.addEventListener( 'click', queryUsers = () => {
+    fetch( "api/v1/auth//user/username/" + userSearch.value, {
         method: "GET",
         headers: { "Content-Type": "application/json" },
     } )
         .then( ( response ) =>  response.json() )
-        .then(  data  => console.log( data ) );
+        .then( ( response ) => {
+
+            for (i = 0; i < response.length; i++)
+            {
+                var data = response[i];
+                if (!(displayedUsers.has(data.username)))
+                {
+                    var div = document.createElement('div');
+                    div.textContent = response[i];
+                    userContainer.appendChild(div);
+                    displayedUsers.add(data.username);
+                }
+            }
+        });
 } );
