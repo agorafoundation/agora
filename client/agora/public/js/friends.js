851f1482f6f5b9c6b0514c1eec81459e7758be54
youssefa123 <yabdelhady905@gmail.com>
2023-10-23 00:57:59 -0400

    Restored the orginal friend.js.

--- client/agora/public/js/friends.js
+++ client/agora/public/js/friends.js
@@ -12,60 +12,13 @@ const templateContainer = document.getElementById( 'user-container' );
 
 
 searchButton.addEventListener( 'click', queryUsers = () => {
-    fetch( "api/v1/auth//user/username/" + userSearch.value, {
+    const response = fetch( "api/v1/auth//user/username/" + 
+userSearch.value, {
         method: "GET",
         headers: { "Content-Type": "application/json" },
     } )
-        .then( ( response ) =>  response.json() )
-        .then( ( response ) => {
 
-            for ( i = 0; i < response.length; i++ ) {
-                var data = response[i];
-                if ( !(displayedUsers.has(data.username)) )
-                {
-                    var newContainer = templateContainer.cloneNode(true);
-                    newContainer.style.display = "inline-block";
-                    friendsContainer.appendChild(newContainer);
-                    displayedUsers.add(data.username);
-                }
-            }
-        } );
-} );
+    .then( ( response ) =>  response.json() )
+    .then(  data  => console.log( data ) );
+} ); 
 
-/*
-// Event listener for "Accept" and "Deny" buttons within the modal
-friendRequestsModal.addEventListener( 'click', ( event ) => {
-    if ( event.target.classList.contains( 'accept-friend-request' ) ) {
-        const requestId = event.target.getAttribute( 'data-id' );
-        fetch( `/api/acceptFriendRequest/${requestId}`, {
-            method: 'POST',
-        } )
-            .then( ( response ) => {
-                if ( response.ok ) {
-                // Update the UI as needed (e.g., remove the request item)
-                    event.target.closest( '.friend-request-item' ).remove();
-                }
-            } )
-            .catch( ( error ) => {
-                console.error( 'Error accepting friend request:', error );
-            } );
-    }
-    else if ( event.target.classList.contains( 'deny-friend-request' ) ) {
-        const requestId = event.target.getAttribute( 'data-id' );
-        // Handle the "Deny" action here (e.g., send a request to deny the friend request).
-        // You can use AJAX to communicate with the server.
-        fetch( `/api/rejectFriendRequest/${requestId}`, {
-            method: 'POST',
-        } )
-            .then( ( response ) => {
-                if ( response.ok ) {
-                // Update the UI as needed (e.g., remove the request item)
-                    event.target.closest( '.friend-request-item' ).remove();
-                }
-            } )
-            .catch( ( error ) => {
-                console.error( 'Error denying friend request:', error );
-            } );
-    }
-} );
-*/
