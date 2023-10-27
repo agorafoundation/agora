887d613d799beddd5135d9c0dfcb527bab41131e
Brian Gormanly <brian.gormanly@marist.edu>
2023-10-27 12:17:45 -0400

    resolved conflict with response hooks

--- server/agora.js
+++ server/agora.js
@@ -14,8 +14,7 @@ require( "dotenv" ).config();
 
 // manage parsing json from body of the request
 const bodyParser = require( "body-parser" );
-// library that allows us to hook responses in the middleware
-const responseHooks = require( "express-response-hooks" );
+
 var path = require( "path" );
 
 app.use(
@@ -24,6 +23,10 @@ app.use(
     } )
 );
 app.use( bodyParser.json() );
+
+
+// library that allows us to hook responses in the middleware
+const responseHooks = require( "express-response-hooks" );
 
 app.use( responseHooks() );
 
