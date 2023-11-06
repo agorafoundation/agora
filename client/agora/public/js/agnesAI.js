1daeb295c5cd2884d4d92dd05663f3ff01188bf2
B-cabrera <brendenc29@gmail.com>
2023-11-06 14:49:13 -0500

    add citation formatting functionality w/ dropdown

--- client/agora/public/js/agnesAI.js
+++ client/agora/public/js/agnesAI.js
@@ -1,10 +1,43 @@
 
 // Citations logic
+
+// citation dropdown functionality
 const citationsDropdown = document.getElementById( 'citations-dropdown' );
 
 citationsDropdown.addEventListener( 'change', ( event ) => {
-    // temp behavior, logging selected type
-    console.log( `Citation type: ${event.target.value}` );
+    const citationType = event.target.value;
+    const articleInfoObj = JSON.parse( localStorage.getItem( 'last-retrieved' ) ?? 'null' );
+  
+    // return here if there is no article info in localstorage
+    if ( !articleInfoObj ) return;
+  
+    // get all card text elements
+    const allCitationCards = document.querySelectorAll( 'span.card-citation-text' );
+    
+    const allCitations = articleInfoObj.citations;
+    allCitationCards.forEach( ( cardTextElement, index ) => {
+        cardTextElement.textContent = formatCitationByType( allCitations[index], citationType );
+    } );
 
-    // TODO: format article information based on selected type
 } );
+
+function formatCitationByType( articleObj, citationFormat ) {
+    const {title, authors, publication, publicationDate } = articleObj;
+
+    switch ( citationFormat ) { // formatting here is weird!
+
+    case 'apa': 
+        return `${authors}. ${publicationDate}. ${title}. ${publication}`;
+
+    case 'mla': 
+        return `${authors}. ${title}. ${publication}, ${publicationDate}`;
+
+    case 'harvard': 
+        return `${authors}, ${publicationDate}. ${title}. ${publication}`;
+
+    case 'chicago': 
+        return `${authors}. ${title}. ${publication}. ${publicationDate}`;
+
+    }
+
+}
