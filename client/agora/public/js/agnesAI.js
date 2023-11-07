6127a8b0b93964b42096bb2a90c850784e506f80
hduale13 <hodo.duale1@marist.edu>
2023-11-07 14:16:27 -0500

    Committing changes to modal header

--- client/agora/public/js/agnesAI.js
+++ client/agora/public/js/agnesAI.js
@@ -8,3 +8,27 @@ citationsDropdown.addEventListener( 'change', ( event ) => {
 
     // TODO: format article information based on selected type
 } );
+
+
+// Dropdown logic
+document.getElementById('doc-type').addEventListener('change', function () {
+    var selectedValue = this.value;
+    var selectedContent = document.getElementById('selectedContent');
+    if (selectedValue === 'notes' || selectedValue === 'paper') {
+        selectedContent.classList.remove('hidden');
+    } else {
+        selectedContent.classList.add('hidden');
+    }
+});
+
+// Popover logic
+var myPopover = new bootstrap.Popover(document.getElementById('myPopover'), {
+    trigger: 'manual'
+});
+document.getElementById('myPopover').addEventListener('mouseenter', function () {
+    myPopover.show();
+});
+document.getElementById('myPopover').addEventListener('mouseleave', function () {
+    myPopover.hide();
+});
+
