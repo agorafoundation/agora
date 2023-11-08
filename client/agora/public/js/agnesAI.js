f36af0bffa984039946e22a3c5c9dac75d22f03d
Kyle <kyledoescode@gmail.com>
2023-11-08 09:46:08 -0500

    Fixed formatting in Agnes JS file

--- client/agora/public/js/agnesAI.js
+++ client/agora/public/js/agnesAI.js
@@ -11,24 +11,25 @@ citationsDropdown.addEventListener( 'change', ( event ) => {
 
 
 // Dropdown logic
-document.getElementById('doc-type').addEventListener('change', function () {
+document.getElementById( 'doc-type' ).addEventListener( 'change', function () {
     var selectedValue = this.value;
-    var selectedContent = document.getElementById('selectedContent');
-    if (selectedValue === 'notes' || selectedValue === 'paper') {
-        selectedContent.classList.remove('hidden');
-    } else {
-        selectedContent.classList.add('hidden');
+    var selectedContent = document.getElementById( 'selectedContent' );
+    if ( selectedValue === 'notes' || selectedValue === 'paper' ) {
+        selectedContent.classList.remove( 'hidden' );
     }
-});
+    else {
+        selectedContent.classList.add( 'hidden' );
+    }
+} );
 
 // Popover logic
-var myPopover = new bootstrap.Popover(document.getElementById('myPopover'), {
+var myPopover = new bootstrap.Popover( document.getElementById( 'myPopover' ), {
     trigger: 'manual'
-});
-document.getElementById('myPopover').addEventListener('mouseenter', function () {
+} );
+document.getElementById( 'myPopover' ).addEventListener( 'mouseenter', function () {
     myPopover.show();
-});
-document.getElementById('myPopover').addEventListener('mouseleave', function () {
+} );
+document.getElementById( 'myPopover' ).addEventListener( 'mouseleave', function () {
     myPopover.hide();
-});
+} );
 
