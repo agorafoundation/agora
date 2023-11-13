4cc8d916e1f1d74952d2e12ec120c51aaafae930
kyliewasserman <79178779+kyliewasserman@users.noreply.github.com>
2023-11-13 14:07:10 -0500

    fixed button

--- client/agora/public/js/agnesAI.js
+++ client/agora/public/js/agnesAI.js
@@ -37,22 +37,26 @@ document.querySelectorAll( 'button.card-cite-btn' ).forEach( ( copyButton ) => {
 
 // get all the copy button text and add the click event listener
 document.querySelectorAll( 'button.card-cite-btn' ).forEach( ( copyButton ) => {
+
+    console.log( "here" );
     copyButton.addEventListener( 'click', function () {
 
-        document.querySelectorAll( 'button.card-citation-text' ).forEach( ( copyText ) => {
-            const cardTextDiv = this.parentElement.parentElement.parentElement;
-            const cardTextID = cardDiv.getAttribute( 'data-card-index-text' );
-        } );
         // the x-button is nested in two divs, so parent div is three levels up
         const cardDiv = this.parentElement.parentElement.parentElement;
-        const cardID = cardDiv.getAttribute( 'data-card-index' );
+        //const cardID = cardDiv.getAttribute( 'data-card-index' );
 
-        copyContentText( cardID );
+        const cardText = cardDiv.querySelector( 'span.card-citation-text' );
+        //const cardTextID = cardTextDiv.getAttribute( 'data-card-index' );
+        //console.log( cardText );
+        //console.log( cardTextID );
+
+        copyContentText( cardText.textContent, cardDiv );
+        //copyContent();
         
     } );
 } );
 
-const createToast = ( text ) => {
+const createToastNotification = ( text ) => {
     document.getElementById( 'toast-text' ).innerText = text;
     const thisToast = document.getElementById( 'liveToast' );
     // eslint-disable-next-line no-undef
@@ -60,21 +64,26 @@ const createToast = ( text ) => {
     toast.show();
 };
 
-function copyContentText ( cardID, cardTextID ) {
-    let text = document.getElementById( cardTextID ).innerHTML;
-    let buttonText = document.getElementById( cardID );
+function copyContentText ( cardText, cardDiv ) {
+    //let text = document.getElementById( cardTextID ).innerHTML;
+    //let buttonText = document.getElementById( cardTextID );
+
+    let buttonText = cardDiv.querySelector( 'button.card-cite-btn' );
+
     const copyContent = async () => {
         try {
-            await navigator.clipboard.writeText( text );
+            await navigator.clipboard.writeText( cardText );
             console.log( 'Content copied to clipboard' );
             buttonText.innerHTML = 'Copied';
-            createToast( "Copied Link! Paste citation into document where needed." );
+            createToastNotification( "Copied Link! Paste citation into document where needed." );
         } 
         catch ( err ) {
             console.error ( 'Failed to copy: ', err );
         }
     };
+    copyContent();
 }
+
 
 /**
  * Formats an article object based on citation format.
